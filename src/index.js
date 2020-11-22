dcf3d53002041c7a7e80e2922c61c3180b88cc5f
mollymoog <mmkover@gmail.com>
2020-11-22 11:13:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("submit").addEventListener("click", function(event) { 
+    const task = document.getElementById("new-task-description").value;
+
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+
+    ul.append(task, li);
+
+    event.target.removeEventListener;
+    event.preventDefault();
+  }, false);
+  
+
 });
